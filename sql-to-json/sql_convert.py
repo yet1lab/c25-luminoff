7b77b0d5660caa16c6da5d563b7b0b8978095651
caas-lu3 <lucas.spinola@ufrpe.br>
2025-12-01 21:31:11 -0300

    estruturação do iterador da tabela de horários

--- sql-to-json/sql_convert.py
+++ sql-to-json/sql_convert.py
@@ -1,18 +1,18 @@
 import sqlite3
 import pandas as pd #
 
-conn = sqlite3.connect("db.sqlite3")
+conn = sqlite3.connect("db copy.sqlite3")
 
 cursor = conn.cursor()
-# cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
 
-cursor.execute(
+'''cursor.execute(
 """
 SELECT core_horarioturma.id, core_horarioturma.dia_semana, core_horarioturma.hora_inicio, core_horarioturma.hora_fim,
 core_sala.nome as id_da_sala, core_horarioturma.turma_id FROM core_horarioturma left join
 core_sala on core_horarioturma.sala_id = core_sala.id;
 """
-)
+)'''
 
 # cursor.execute("SELECT * FROM core_disciplina;")
 
--- sql-to-json/sql_test2.py
+++ sql-to-json/sql_test2.py
@@ -1,10 +1,18 @@
 import sqlite3
 import pandas as pd #
 
-conn = sqlite3.connect("db.sqlite3")
+conn = sqlite3.connect("db copy.sqlite3")
 
+lista_de_horarios_fim = ['18:30:00', '20:10:00']  # Exemplo de lista de horários de fim
 
-sql = "SELECT core_horarioturma.id, core_horarioturma.dia_semana, core_horarioturma.hora_inicio, core_horarioturma.hora_fim, core_sala.nome as id_da_sala, core_horarioturma.turma_id FROM core_horarioturma left join core_sala on core_horarioturma.sala_id = core_sala.id;"
+z = "20:10:00"  # Exemplo de horário específico
+
+sql = F"""
+SELECT 
+    count(*)
+FROM core_horarioturma LEFT JOIN core_sala ON core_horarioturma.sala_id = core_sala.id
+WHERE core_horarioturma.hora_fim IN ('{z}');
+"""
 
 df = pd.read_sql_query(sql, conn) 
 
